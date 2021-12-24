0155a8fc1811f8d51dd2f253058b4eefe22c1cd0
Bodi <bodiaugustus@outlook.com>
2021-12-24 14:26:47 -0600

    Added getStaticPaths function to slug.js to get an object list of params containing slugs, one per page on the site - one per product. Then c&p getStaticProps in from index.js and used to get a single course and then compare that courses slug id with the params slug id and return if it they match.

--- pages/courses/[slug].js
+++ pages/courses/[slug].js
@@ -5,6 +5,7 @@ import {
   Keypoints
 } from "@components/course";
 import { BaseLayout } from "@components/layout";
+import { getAllCourses } from "@content/courses/fetcher";
 
 export default function Course() {
 
@@ -19,5 +20,29 @@ export default function Course() {
     </>
   )
 }
+
+export function getStaticPaths(){
+
+    const {data} = getAllCourses()
+
+    return {
+        paths: data.map(course => ({
+            params: {
+                slug: course.slug
+            }
+        })),
+        fallback: false
+    }
+}
+
+export function getStaticProps({params}){
+    const {data} = getAllCourses()
+    const course = data.filter(course => course.slug === params.slug)[0]
+    return {
+        props: {
+          course
+        }
+    }
+  }
 
 Course.Layout = BaseLayout
