fdcbd363efde923163ea54a6c983690ac953a06a
Bodi <bodiaugustus@outlook.com>
2021-12-24 16:13:32 -0600

    Connected the connect button with metamask using the _web3Api inside of a newly constructed useMemo.

--- components/ui/common/navbar/index.js
+++ components/ui/common/navbar/index.js
@@ -1,9 +1,12 @@
 
 
 
+
+import { useWeb3 } from "@components/providers"
 import Link from "next/link"
 
 export default function Footer() {
+  const { connect } = useWeb3()
 
   return (
     <section>
@@ -11,24 +14,37 @@ export default function Footer() {
         <nav className="relative" aria-label="Global">
           <div className="flex justify-between">
             <div>
-            <Link href="/">
-              <a  className="font-medium mr-8 text-gray-500 hover:text-gray-900">Home</a>
-            </Link>
-            <Link href="/marketplace">
-              <a  className="font-medium mr-8 text-gray-500 hover:text-gray-900">Marketplace</a>
-            </Link>
-            <Link href="/blogs">
-              <a  className="font-medium mr-8 text-gray-500 hover:text-gray-900">Blogs</a>
-            </Link>
+              <Link href="/" >
+                <a
+                  className="font-medium mr-8 text-gray-500 hover:text-gray-900">
+                  Home
+                </a>
+              </Link>
+              <Link href="/" >
+                <a
+                  className="font-medium mr-8 text-gray-500 hover:text-gray-900">
+                  Marketplace
+                </a>
+              </Link>
+              <Link href="/" >
+                <a
+                  className="font-medium mr-8 text-gray-500 hover:text-gray-900">
+                  Blogs
+                </a>
+              </Link>
             </div>
             <div>
-            <Link href="/">
-              <a  className="font-medium mr-8 text-gray-500 hover:text-gray-900">Wishlist</a>
-            </Link>
-
-
-              <a href="#" className="px-8 py-3 border text-base rounded-md font-medium text-white bg-indigo-600  hover:bg-indigo-700">Connect</a>
-    
+              <Link href="/" >
+                <a
+                  className="font-medium mr-8 text-gray-500 hover:text-gray-900">
+                  Wishlist
+                </a>
+              </Link>
+              <span
+                onClick={connect}
+                className="px-8 py-3 border rounded-md text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700">
+                  Connect
+              </span>
             </div>
           </div>
         </nav>
